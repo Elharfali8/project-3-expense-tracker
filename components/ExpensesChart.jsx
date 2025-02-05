b9906d926285ece777df51ed83b6412e27403025
Elharfali <0631173826you@gmail.com>
2025-02-05 19:33:23 +0100

    Dashboard page : Add data to the chart

--- components/ExpensesChart.jsx
+++ components/ExpensesChart.jsx
@@ -1,23 +1,31 @@
 'use client'
+import { useSelector } from "react-redux";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
-const data = [
-  { name: "Page A", pv: 2400, uv: 4000 },
-  { name: "Page B", pv: 1398, uv: 3000 },
-  { name: "Page C", pv: 9800, uv: 2000 },
-];
+
 
 const ExpenseChart = () => {
+  const { expenses } = useSelector((store) => store.expense)
+  
+  const expenseData = expenses.reduce((acc, expense) => {
+    const existingCategory = acc.find(item => item.category === expense.category);
+    if (existingCategory) {
+      existingCategory.amount += Number(expense.amount); // Sum expenses per category
+    } else {
+      acc.push({ category: expense.category, amount: Number(expense.amount) });
+    }
+    return acc;
+  }, []);
+
   return (
     <div className="bg-muted p-6 rounded-lg shadow-md">
       <h3 className="text-lg font-semibold">Expense Chart</h3>
       <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data}>
-          <XAxis dataKey="name" />
+        <BarChart data={expenseData}>
+          <XAxis dataKey="category" />
           <YAxis />
           <Tooltip />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
+          <Bar dataKey="amount" fill="#8884d8" />
         </BarChart>
       </ResponsiveContainer>
     </div>
